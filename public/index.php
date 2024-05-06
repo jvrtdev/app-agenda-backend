19a3cdd0d775f9ec53023dc22739eb1ae9d22818
jvrtdevmac <joaovitorromanoteixeira@gmail.com>
2024-05-06 01:19:05 -0300

    :recycle: refactor: alterando campos para o cors permitir as rotas

--- public/index.php
+++ public/index.php
@@ -1,21 +1,15 @@
 <?php
-
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use App\Controllers\AgendaController;
 use App\Controllers\UserController;
 use Middleware\JwtMiddleware;
 use Slim\Factory\AppFactory;
-// use Slim\Routing\RouteCollectorProxy;
-// use Slim\Routing\RouteContext;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-// use Psr\Http\Server\RequestHandlerInterface;
-
-
+use Slim\Psr7\Response;
+use Slim\Routing\RouteCollectorProxy;
+use Slim\Routing\RouteContext;
 
 require __DIR__ . '/../vendor/autoload.php';
-
-
-
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
@@ -24,50 +18,37 @@ $dotenv->load();
 $app = AppFactory::create();
 //middleware do CORS
 $app->add(require __DIR__ . '/../middleware/cors.php');
+$app->add(new Middleware\CorsMiddleware());
 
-// Rota OPTIONS genérica para lidar com solicitações OPTIONS
-$app->options('/{routes:.*}', function (Request $request, Response $response) {
+//autoriza a rota options 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
   return $response;
 });
-// $app->addBodyParsingMiddleware();
 
-// // This middleware will append the response header Access-Control-Allow-Methods with all allowed methods
-// $app->add(function (Request $request, RequestHandlerInterface $handler): Response {
-//   $response = $handler->handle($request);
+//funcao para habilitar o cors
+$app->add(function ($request, $handler) {
+  $response = $handler->handle($request);
+  return $response
+    ->withHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
 
-//   $response = $response->withHeader('Access-Control-Allow-Origin', '*');
-//   $response = $response->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   $response = $response->withHeader('Access-Control-Allow-Headers', '*');
 
-//   // Optional: Allow Ajax CORS requests with Authorization header
-//   $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
-
-//   return $response;
-// });
-
-// // The RoutingMiddleware should be added after our CORS middleware so routing is performed first
-// $app->addRoutingMiddleware();
-$app->post('/user/create', UserController::class . ':createUser');
-$app->post('/user/login', UserController::class . ':loginUser');
-$app->post('/teste', UserController::class . ':testToken');
 
 //get
 $app->get('/', UserController::class . ':teste');
-
-$app->group('/api', function ($group) {
+//rotas sem JWT
+$app->post('/api/user/create', UserController::class . ':createUser');
+$app->post('/api/user/login', UserController::class . ':loginUser');
+$app->post('/teste', UserController::class . ':testToken');
+//rotas protegidas com jwt
+$app->group('/api', function (RouteCollectorProxy $group) {
   $group->get('/users', UserController::class . ':getUsers');
   $group->get('/bookings', AgendaController::class . ':getAllBookings');
   $group->post('/booking/create', AgendaController::class . ':createBooking');
   $group->delete('/user/delete', UserController::class . ':deleteUser');
+})->add(new JwtMiddleware());
   
-  
-})->add(new JwtMiddleware("minhachavesecreta"));
-  
-
-//post
-
-
-//gelete
-
 
 $app->run();
