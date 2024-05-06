5d7ab6e945aa672a98bb126b2de783ba451ba14c
jvrtdevmac <joaovitorromanoteixeira@gmail.com>
2024-05-06 01:20:30 -0300

    :recycle: refactor: simplificando middlewarejwt auth

--- middleware/JwtMiddleware.php
+++ middleware/JwtMiddleware.php
@@ -1,43 +1,4 @@
 <?php 
-/*
-namespace Middleware;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
-use Psr\Http\Message\ResponseInterface as Response;
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
-
-class JwtMiddleware
-{
-    protected $secret_key;
-
-    public function __construct()
-    {
-        $this->secret_key = "minhachavesecreta";
-    }
-
-    public function __invoke(Request $request,Response $response, RequestHandler $handler): Response
-    {
-        $token = $request->getHeaderLine('Authorization');
-        if (!$token) {
-          $response->getBody()->write(json_encode(["Erro" => "Token inexistente"]));
-          return $response->withStatus(401)->withHeader('Content-type','application/json');
-        }
-
-        try {
-            $decoded = JWT::decode($token, new Key($this->secret_key, 'HS256'));
-            $request = $request->withAttribute('token', $decoded);
-            $response->getBody()->write(json_encode($decoded));
-
-            return $handler->handle($request);
-            
-        } catch (\Exception $e) {
-            $response->getBody()->write(json_encode(["Erro" => "Token invalido"]));
-            return $response->withStatus(401)->withHeader('Content-type','application/json');
-            
-        }
-    }
-} */
 namespace Middleware;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -51,7 +12,7 @@ class JwtMiddleware
 {
     protected string $secret_key;
 
-    public function __construct($secret_key)
+    public function __construct()
     {
       $this->secret_key = "minhachavesecreta";
     }
@@ -59,24 +20,25 @@ class JwtMiddleware
     public function __invoke(Request $request, RequestHandler $handler): Response
     {
         $token = $request->getHeaderLine('Authorization');
-        
       if ($token) {
         if (strpos($token, 'Bearer ') === 0) {
           // Remove o prefixo "Bearer " da string
           $token = substr($token, 7);
       }
+      $token = trim($token);
+      
 
         try {
             $decoded = JWT::decode($token, new Key($this->secret_key, 'HS256'));
-            $request = $request->withAttribute('token', $decoded);
-
-            $response = new \Slim\Psr7\Response();
-            $response->getBody()->write(json_encode($decoded));
+            //$request = $request->withAttribute('token', $decoded);
+            
+            //$response = new \Slim\Psr7\Response();
+            //$response->getBody()->write(json_encode($decoded));
             return $handler->handle($request);
         } catch (Exception $e) {
             $response = new \Slim\Psr7\Response();
             print_r($e->getMessage());
-            $response->getBody()->write(json_encode(['error' => $token]));
+            $response->getBody()->write(json_encode(['error' => 'Erro ao autenticar ']));
             return $response->withStatus(401)->withHeader('Content-Type', 'application/json');
         }
       }
